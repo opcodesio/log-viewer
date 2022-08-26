0d58e23add4d485da676bc094d7430094ab2604e
arukompas <arukompas@users.noreply.github.com>
2022-08-26 12:22:17 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -1,17 +1,17 @@
 <?php
 
-if (!function_exists('highlight_search_result')) {
+if (! function_exists('highlight_search_result')) {
     /**
      * Highlights search query results and escapes HTML.
      * Safe to use within {!! !!} in Blade.
      *
-     * @param string $text
-     * @param string|null $query
+     * @param  string  $text
+     * @param  string|null  $query
      * @return string
      */
     function highlight_search_result(string $text, string $query = null): string
     {
-        if (!empty($query)) {
+        if (! empty($query)) {
             if (! \Illuminate\Support\Str::endsWith($query, '/i')) {
                 $query = '/'.$query.'/i';
             }
@@ -19,12 +19,11 @@ if (!function_exists('highlight_search_result')) {
             $text = preg_replace_callback(
                 $query,
                 function ($matches) {
-                    return '<mark>' . $matches[0] . '</mark>';
+                    return '<mark>'.$matches[0].'</mark>';
                 },
                 $text
             );
         }
-
 
         // Let's return the <mark> tags which we use for highlighting the search results
         // while escaping the rest of the HTML entities
