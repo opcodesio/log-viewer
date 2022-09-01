3038dd856b3d559446cd066c561bae24be42a3ca
arukompas <arukompas@users.noreply.github.com>
2022-09-01 05:43:03 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -35,25 +35,25 @@ if (! function_exists('highlight_search_result')) {
     }
 }
 
-if (!function_exists('bytes_formatted')) {
+if (! function_exists('bytes_formatted')) {
     /**
      * Get a human-friendly readable string of the number of bytes provided.
      */
     function bytes_formatted(int $bytes): string
     {
         if ($bytes > ($gb = 1024 * 1024 * 1024)) {
-            return number_format($bytes / $gb, 2) . ' GB';
+            return number_format($bytes / $gb, 2).' GB';
         } elseif ($bytes > ($mb = 1024 * 1024)) {
-            return number_format($bytes / $mb, 2) . ' MB';
+            return number_format($bytes / $mb, 2).' MB';
         } elseif ($bytes > ($kb = 1024)) {
-            return number_format($bytes / $kb, 2) . ' KB';
+            return number_format($bytes / $kb, 2).' KB';
         }
 
-        return $bytes . ' bytes';
+        return $bytes.' bytes';
     }
 }
 
-if (!function_exists('size_of_var')) {
+if (! function_exists('size_of_var')) {
     /**
      * Calculate the memory footprint of a given variable.
      * CAUTION: This will increase the memory usage by that same amount because it makes a copy of this variable.
@@ -62,11 +62,12 @@ if (!function_exists('size_of_var')) {
     {
         $start_memory = memory_get_usage();
         $tmp = unserialize(serialize($var));
+
         return memory_get_usage() - $start_memory;
     }
 }
 
-if (!function_exists('size_of_var_in_mb')) {
+if (! function_exists('size_of_var_in_mb')) {
     /**
      * Calculate the memory footprint of a given variable and return it as a human-friendly string.
      * CAUTION: This will increase the memory usage by that same amount because it makes a copy of this variable.
