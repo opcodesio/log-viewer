cdbe5463488933afbb7822abc2554804668abfde
arukompas <arukompas@users.noreply.github.com>
2022-09-02 08:40:21 +0000

    Fix styling

--- tests/Unit/ProcessingLaravelLogEntriesTest.php
+++ tests/Unit/ProcessingLaravelLogEntriesTest.php
@@ -1,13 +1,13 @@
 <?php
 
-use Opcodes\LogViewer\Log;
 use Opcodes\LogViewer\Level;
+use Opcodes\LogViewer\Log;
 use function PHPUnit\Framework\assertEquals;
 
 it('can understand the default Laravel log format', function () {
-    $text = "[2022-08-25 11:16:17] local.DEBUG: Example log entry for the level debug";
+    $text = '[2022-08-25 11:16:17] local.DEBUG: Example log entry for the level debug';
 
-    $log = new Log($index = 10, $text, $fileName = "laravel.log", $filePosition = 5200);
+    $log = new Log($index = 10, $text, $fileName = 'laravel.log', $filePosition = 5200);
 
     assertEquals($index, $log->index);
     assertEquals(Level::Debug, $log->level->value);
@@ -20,13 +20,13 @@ it('can understand the default Laravel log format', function () {
 });
 
 it('can understand multi-line logs', function () {
-    $logText = <<<EOF
+    $logText = <<<'EOF'
 Example log entry for the level debug
 with multiple lines of content.
 {"one":1,"two":"two","three":[1,2,3]}
 can contain dumped objects or JSON as well - it's all part of the contents.
 EOF;
-    $text = "[2022-08-25 11:16:17] local.DEBUG: ".$logText;
+    $text = '[2022-08-25 11:16:17] local.DEBUG: '.$logText;
 
     $log = new Log(0, $text, 'laravel.log', 0);
 
@@ -35,7 +35,7 @@ EOF;
 });
 
 it('can understand the optional microseconds in the timestamp', function () {
-    $text = "[2022-08-25 11:16:17.125000] local.DEBUG: Example log entry for the level debug";
+    $text = '[2022-08-25 11:16:17.125000] local.DEBUG: Example log entry for the level debug';
 
     $log = new Log(0, $text, 'laravel.log', 0);
 
@@ -43,7 +43,7 @@ it('can understand the optional microseconds in the timestamp', function () {
 });
 
 it('can understand the optional time offset in the timestamp', function () {
-    $text = "[2022-08-25 11:16:17.125000+02:00] local.DEBUG: Example log entry for the level debug";
+    $text = '[2022-08-25 11:16:17.125000+02:00] local.DEBUG: Example log entry for the level debug';
     $expectedTimestamp = 1661418977;
 
     $log = new Log(0, $text, 'laravel.log', 0);
@@ -52,7 +52,7 @@ it('can understand the optional time offset in the timestamp', function () {
 
     // Meanwhile, if we switch the time offset an hour forward,
     // we can expect the timestamp to be reduced by 3600 seconds.
-    $text = "[2022-08-25 11:16:17.125000+03:00] local.DEBUG: Example log entry for the level debug";
+    $text = '[2022-08-25 11:16:17.125000+03:00] local.DEBUG: Example log entry for the level debug';
     $newExpectedTimestamp = $expectedTimestamp - 3600;
 
     $log = new Log(0, $text, 'laravel.log', 0);
@@ -61,13 +61,13 @@ it('can understand the optional time offset in the timestamp', function () {
 });
 
 it('can handle text in-between timestamp and environment/severity', function () {
-    $text = "[2022-08-25 11:16:17] some additional text [] and characters // !@#$ local.DEBUG: Example log entry for the level debug";
+    $text = '[2022-08-25 11:16:17] some additional text [] and characters // !@#$ local.DEBUG: Example log entry for the level debug';
     $expectedAdditionalText = 'some additional text [] and characters // !@#$';
 
     $log = new Log(0, $text, 'laravel.log', 0);
 
-    assertEquals($expectedAdditionalText . ' Example log entry for the level debug', $log->text);
-    assertEquals($expectedAdditionalText . ' Example log entry for the level debug', $log->fullText);
+    assertEquals($expectedAdditionalText.' Example log entry for the level debug', $log->text);
+    assertEquals($expectedAdditionalText.' Example log entry for the level debug', $log->fullText);
     // got to make sure the rest of the data is still processed correctly!
     assertEquals('local', $log->environment);
     assertEquals(Level::Debug, $log->level->value);
