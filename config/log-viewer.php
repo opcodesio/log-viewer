c35dd870154b5f281ec9589f6c9d4242c1863b10
arukompas <arukompas@users.noreply.github.com>
2022-11-22 05:54:32 +0000

    Fix styling

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -122,8 +122,8 @@ return [
              * $matches[7] - the log text, the rest of the text.
              */
             'log_parsing_regex' => '/^\[(\d{4}-\d{2}-\d{2}[T ]\d{2}:\d{2}:\d{2}\.?(\d{6}([\+-]\d\d:\d\d)?)?)\](.*?(\w+)\.|.*?)('
-                . implode('|', array_filter(Level::caseValues()))
-                . ')?: (.*?)( in [\/].*?:[0-9]+)?$/is',
+                .implode('|', array_filter(Level::caseValues()))
+                .')?: (.*?)( in [\/].*?:[0-9]+)?$/is',
         ],
     ],
 
