fe94f4400aea082d141512a30467b5ece5483908
arukompas <arukompas@users.noreply.github.com>
2023-02-11 07:01:41 +0000

    Fix styling

--- src/Http/Resources/LevelCountResource.php
+++ src/Http/Resources/LevelCountResource.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class LevelCountResource extends JsonResource
 {
     /**
-     * @param Request $request
+     * @param  Request  $request
      * @return array
      */
     public function toArray($request)
