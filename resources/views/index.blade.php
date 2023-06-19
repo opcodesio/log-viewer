8450910a2f30c7e61bd0bc203d8e2cd9361c2ed1
corean <corean@corean.biz>
2023-06-19 21:54:02 +0900

    appo locale set-up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
