87a4c55af68d1c384e7e4e46f0e29da0e839c219
arukompas <arukompas@users.noreply.github.com>
2023-07-10 09:20:47 +0000

    Fix styling

--- src/Log.php
+++ src/Log.php
@@ -152,7 +152,7 @@ class Log
 
             if (json_last_error() == JSON_ERROR_CTRL_CHAR) {
                 // might need to escape new lines
-                $json_data = json_decode(str_replace("\n", "\\n", $json_string), true);
+                $json_data = json_decode(str_replace("\n", '\\n', $json_string), true);
             }
 
             if (json_last_error() == JSON_ERROR_NONE) {
