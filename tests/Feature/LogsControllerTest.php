b0258be17296d12ef33acc60801f6e8d6c736176
Arunas Skirius <arukomp@gmail.com>
2023-07-28 09:19:58 +0300

    remove dumps

--- tests/Feature/LogsControllerTest.php
+++ tests/Feature/LogsControllerTest.php
@@ -9,13 +9,8 @@ it('can load the logs for a specific file', function () {
         makeLaravelLogEntry(),
     ];
     $file = generateLogFile('test.log', implode(PHP_EOL, $logEntries));
-    dump($file);
-    dump(\Opcodes\LogViewer\Facades\LogViewer::getFiles());
 
     $response = getJson(route('log-viewer.logs', ['file' => $file->identifier]));
-
-    dump($file->contents());
-    dump($response->json());
 
     expect($response->json('logs'))->toHaveCount(count($logEntries));
 });
