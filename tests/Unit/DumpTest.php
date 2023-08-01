9a1a4d3c56c5afcf443fb69830a569147a85375f
Arunas Skirius <arukomp@gmail.com>
2023-08-01 15:09:08 +0300

    exclude testing Laravel 10 on PHP 8.0

--- tests/Unit/DumpTest.php
+++ tests/Unit/DumpTest.php
@@ -1,5 +1,10 @@
 <?php
 
-test('dumps')
-    ->expect(['dump', 'dd'])
-    ->toOnlyBeUsedIn('Opcodes\LogViewer\Utils\Benchmark');
+test('dumps', function () {
+    if (version_compare(\Pest\version(), '2.0.0', '<')) {
+        $this->markTestSkipped('This test is only for Pest 2.0.0+');
+    }
+
+    expect(['dump', 'dd'])
+        ->toOnlyBeUsedIn('Opcodes\LogViewer\Utils\Benchmark');
+});
