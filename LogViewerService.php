384c1b2d697d32daf5a1aeee2faa2ae393bad016
mohammadRezaPegah <117629765+mohammadRezaPegah@users.noreply.github.com>
2023-12-20 10:47:06 +0330

    fix: read mix-manifest.json
    check mix-manifest.json from public folder on localhost and public_html on server

---
+++ LogViewerService.php
@@ -0,0 +1,331 @@
+<?php
+
+namespace Opcodes\LogViewer;
+
+use Composer\InstalledVersions;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
+use Opcodes\LogViewer\Readers\IndexedLogReader;
+use Opcodes\LogViewer\Readers\LogReaderInterface;
+use Opcodes\LogViewer\Utils\Utils;
+
+class LogViewerService
+{
+    const DEFAULT_MAX_LOG_SIZE_TO_DISPLAY = 131_072;    // 128 KB
+
+    public static string $logFileClass = LogFile::class;
+    public static string $logReaderClass = IndexedLogReader::class;
+    protected ?Collection $_cachedFiles = null;
+    protected mixed $authCallback;
+    protected int $maxLogSizeToDisplay = self::DEFAULT_MAX_LOG_SIZE_TO_DISPLAY;
+    protected mixed $hostsResolver;
+    protected string $layout = 'log-viewer::index';
+
+    protected function getLaravelLogFilePaths(): array
+    {
+        // Because we'll use the base path as a parameter for `glob`, we should escape any
+        // glob's special characters and treat those as actual characters of the path.
+        // We can assume this, because it's the actual path of the Laravel app, not a user-defined
+        // search pattern.
+        if (PHP_OS_FAMILY === 'Windows') {
+            $baseDir = str_replace(
+                ['[', ']'],
+                ['{LEFTBRACKET}', '{RIGHTBRACKET}'],
+                str_replace('\\', '/', $this->basePathForLogs())
+            );
+            $baseDir = str_replace(
+                ['{LEFTBRACKET}', '{RIGHTBRACKET}'],
+                ['[[]', '[]]'],
+                $baseDir
+            );
+        } else {
+            $baseDir = str_replace(
+                ['*', '?', '\\', '[', ']'],
+                ['\*', '\?', '\\\\', '\[', '\]'],
+                $this->basePathForLogs()
+            );
+        }
+        $files = [];
+
+        foreach (config('log-viewer.include_files', []) as $pattern) {
+            if (!str_starts_with($pattern, DIRECTORY_SEPARATOR)) {
+                $pattern = $baseDir . $pattern;
+            }
+
+            $files = array_merge($files, $this->getFilePathsMatchingPattern($pattern));
+        }
+
+        foreach (config('log-viewer.exclude_files', []) as $pattern) {
+            if (!str_starts_with($pattern, DIRECTORY_SEPARATOR)) {
+                $pattern = $baseDir . $pattern;
+            }
+
+            $files = array_diff($files, $this->getFilePathsMatchingPattern($pattern));
+        }
+
+        $files = array_map('realpath', $files);
+
+        $files = array_filter($files, 'is_file');
+
+        return array_values(array_reverse($files));
+    }
+
+    protected function getFilePathsMatchingPattern($pattern): array
+    {
+        // The GLOB_BRACE flag is not available on some non GNU systems, like Solaris or Alpine Linux.
+
+        if (str_contains($pattern, '**')) {
+            return Utils::glob_recursive($pattern);
+        }
+
+        return glob($pattern) ?: [];
+    }
+
+    public function basePathForLogs(): string
+    {
+        return Str::finish(realpath(storage_path('logs')), DIRECTORY_SEPARATOR);
+    }
+
+    /**
+     * @return LogFileCollection|LogFile[]
+     */
+    public function getFiles(): LogFileCollection
+    {
+        if (!isset($this->_cachedFiles)) {
+            $fileClass = static::$logFileClass;
+
+            $this->_cachedFiles = (new LogFileCollection($this->getLaravelLogFilePaths()))
+                ->unique()
+                ->map(fn ($filePath) => new $fileClass($filePath))
+                ->values();
+
+            if (config('log-viewer.hide_unknown_files', true)) {
+                $this->_cachedFiles = $this->_cachedFiles->filter(function (LogFile $file) {
+                    return !$file->type()->isUnknown();
+                });
+            }
+        }
+
+        return $this->_cachedFiles;
+    }
+
+    public function getFilesGroupedByFolder(): LogFolderCollection
+    {
+        return LogFolderCollection::fromFiles($this->getFiles());
+    }
+
+    /**
+     * Find the file with the given identifier or file name.
+     */
+    public function getFile(?string $fileIdentifier): ?LogFile
+    {
+        if (empty($fileIdentifier)) {
+            return null;
+        }
+
+        $file = $this->getFiles()
+            ->where('identifier', $fileIdentifier)
+            ->first();
+
+        if (!$file) {
+            $file = $this->getFiles()
+                ->where('name', $fileIdentifier)
+                ->first();
+        }
+
+        return $file;
+    }
+
+    public function getFolder(?string $folderIdentifier): ?LogFolder
+    {
+        return $this->getFilesGroupedByFolder()
+            ->first(function (LogFolder $folder) use ($folderIdentifier) {
+                return (empty($folderIdentifier) && $folder->isRoot())
+                    || $folder->identifier === $folderIdentifier
+                    || $folder->path === $folderIdentifier;
+            });
+    }
+
+    public function supportsHostsFeature(): bool
+    {
+        return class_exists(\GuzzleHttp\Client::class);
+    }
+
+    public function resolveHostsUsing(callable $callback): void
+    {
+        $this->hostsResolver = $callback;
+    }
+
+    public function getHosts(): HostCollection
+    {
+        $hosts = HostCollection::fromConfig(config('log-viewer.hosts', []));
+
+        if (isset($this->hostsResolver)) {
+            $hosts = new HostCollection(
+                call_user_func($this->hostsResolver, $hosts) ?? []
+            );
+
+            $hosts->transform(function ($host, $key) {
+                return is_array($host)
+                    ? Host::fromConfig($key, $host)
+                    : $host;
+            });
+        }
+
+        return $hosts->values();
+    }
+
+    public function getHost(?string $hostIdentifier): ?Host
+    {
+        return $this->getHosts()
+            ->first(fn (Host $host) => $host->identifier === $hostIdentifier);
+    }
+
+    public function clearFileCache(): void
+    {
+        $this->_cachedFiles = null;
+    }
+
+    public function getRouteDomain(): ?string
+    {
+        return config('log-viewer.route_domain');
+    }
+
+    public function getRoutePrefix(): string
+    {
+        return config('log-viewer.route_path', 'log-viewer');
+    }
+
+    public function getRouteMiddleware(): array
+    {
+        return config('log-viewer.middleware', []) ?: ['web'];
+    }
+
+    public function auth($callback = null): void
+    {
+        if (is_null($callback) && isset($this->authCallback)) {
+            $canViewLogViewer = call_user_func($this->authCallback, request());
+
+            if (!$canViewLogViewer) {
+                throw new AuthorizationException('Unauthorized.');
+            }
+        } elseif (is_null($callback) && Gate::has('viewLogViewer')) {
+            Gate::authorize('viewLogViewer');
+        } elseif (!is_null($callback) && is_callable($callback)) {
+            $this->authCallback = $callback;
+        }
+    }
+
+    public function lazyScanChunkSize(): int
+    {
+        return intval(config('log-viewer.lazy_scan_chunk_size_in_mb', 100)) * 1024 * 1024;
+    }
+
+    public function lazyScanTimeout(): float
+    {
+        return 5.0;    // 5 seconds
+    }
+
+    /**
+     * Get the maximum number of bytes of the log that we should display.
+     */
+    public function maxLogSize(): int
+    {
+        return $this->maxLogSizeToDisplay;
+    }
+
+    public function setMaxLogSize(int $bytes): void
+    {
+        $this->maxLogSizeToDisplay = $bytes > 0 ? $bytes : self::DEFAULT_MAX_LOG_SIZE_TO_DISPLAY;
+    }
+
+    public function extend(string $type, string $class): void
+    {
+        app(LogTypeRegistrar::class)->register($type, $class);
+    }
+
+    public function useLogFileClass(string $class): void
+    {
+        // figure out whether the class extends from the LogFile class
+        if (!is_subclass_of($class, LogFile::class)) {
+            throw new \InvalidArgumentException("The class {$class} must extend from the " . LogFile::class . ' class.');
+        }
+
+        static::$logFileClass = $class;
+    }
+
+    public function useLogReaderClass(string $class): void
+    {
+        // figure out whether the class implements the LogReaderInterface
+        $reflection = new \ReflectionClass($class);
+
+        if (!$reflection->implementsInterface(LogReaderInterface::class)) {
+            throw new \InvalidArgumentException("The class {$class} must implement the LogReaderInterface.");
+        }
+
+        static::$logReaderClass = $class;
+    }
+
+    public function logReaderClass(): string
+    {
+        return static::$logReaderClass;
+    }
+
+    public function setViewLayout(string $layout): void
+    {
+        $this->layout = $layout;
+    }
+
+    public function getViewLayout(): string
+    {
+        return $this->layout;
+    }
+
+    /**
+     * Determine if Log Viewer's published assets are up-to-date.
+     *
+     * @throws \RuntimeException
+     */
+    public function assetsAreCurrent(): bool
+    {
+        $publishedPath = public_path('vendor/log-viewer/mix-manifest.json');
+        $publishedPublic_htmlPath = base_path('../public_html/vendor/log-viewer/mix-manifest.json');
+
+        switch (true) {
+            case (File::exists($publishedPath)):
+                return File::get($publishedPath) === File::get(__DIR__ . '/../public/mix-manifest.json');
+                break;
+            case (File::exists($publishedPublic_htmlPath)):
+                return File::get($publishedPublic_htmlPath) === File::get(__DIR__ . '/../public/mix-manifest.json');
+                break;
+
+            default:
+                # code...
+                break;
+        }
+        throw new \RuntimeException('Log Viewer assets are not published. Please run: php artisan vendor:publish --tag=log-viewer-assets --force');
+    }
+
+    /**
+     * Get the current version of the Log Viewer
+     */
+    public function version(): string
+    {
+        if (app()->runningUnitTests()) {
+            return 'unit-tests';
+        }
+
+        if (class_exists(InstalledVersions::class)) {
+            return InstalledVersions::getPrettyVersion('opcodesio/log-viewer') ?? 'dev-main';
+        } else {
+            $composerJson = json_decode(file_get_contents(__DIR__ . '/../composer.json'), true);
+
+            return is_array($composerJson) && isset($composerJson['version'])
+                ? $composerJson['version']
+                : 'dev-main';
+        }
+    }
+}
---
+++ index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('vendor/log-viewer/img/log-viewer-32.png') }}">
+
+    <title>Log Viewer{{ config('app.name') ? ' - ' . config('app.name') : '' }}</title>
+
+    <!-- Style sheets-->
+    <link href="{{ asset('vendor/log-viewer/app.css') }}" rel="stylesheet"
+        onerror="alert('app.css failed to load. Please refresh the page, re-publish Log Viewer assets, or fix routing for vendor assets.')">
+</head>
+
+<body class="h-full px-3 lg:px-5 bg-gray-100 dark:bg-gray-900">
+    <div id="log-viewer" class="flex h-full max-h-screen max-w-full">
+        <router-view></router-view>
+    </div>
+
+    <!-- Global LogViewer Object -->
+    <script>
+        window.LogViewer = @json($logViewerScriptVariables);
+
+        // Add additional headers for LogViewer requests like so:
+        // window.LogViewer.headers['Authorization'] = 'Bearer xxxxxxx';
+    </script>
+    <script src="{{ asset('vendor/log-viewer/app.js') }}"
+        onerror="alert('app.js failed to load. Please refresh the page, re-publish Log Viewer assets, or fix routing for vendor assets.')">
+    </script>
+</body>
+
+</html>
